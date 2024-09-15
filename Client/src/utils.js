f721b8b1f51bc9aca4c080577b0e843a7a308c1c
Aatif-007 <145668612+Aatif-007@users.noreply.github.com>
2024-09-15 17:28:54 +0530

    Update utils.js

--- Client/src/utils.js
+++ Client/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (msg,type) => {
     toast[type](msg);
 }
 
-export const API_URL = "https://task-manager-8yq1jqdho-aatifs-projects-f1d93106.vercel.app"
+export const API_URL = "https://task-manager-3rxo1c22a-aatifs-projects-f1d93106.vercel.app"
